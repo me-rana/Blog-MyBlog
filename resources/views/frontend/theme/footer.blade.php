4743b5b412532c5cd419f578b36d09c73e59b0b6
Rana Bepari <irana.bpr9@gmail.com>
2023-12-03 16:53:27 +0600

    Stable Version Released

--- resources/views/frontend/theme/footer.blade.php
+++ resources/views/frontend/theme/footer.blade.php
@@ -1,6 +1,6 @@
         <!-- Footer-->
         <footer class="py-2 bg-dark">
-            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; <a href="https://ranasvc.com"><img src="https://ranasvc.com/storage/assets/image/Rana_Bepari--copyright.png" height="60px"></a>- 2023</p></div>
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; <a href="https://ranasvc.com"><img src="https://ranasvc.com/storage/image/24648Rana_Bepari--copyright.png" height="60px"></a>- 2023</p></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
